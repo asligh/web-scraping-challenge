574361ec85316b618c722b2bd171bf73c0360f61
Ashley Sligh <asligh1@gmail.com>
2021-12-07 20:43:27 -0500

    After scraping

--- scrape_mars.py
+++ scrape_mars.py
@@ -3,27 +3,29 @@ from bs4 import BeautifulSoup as bs
 import time
 from webdriver_manager.chrome import ChromeDriverManager
 
-mars_facts={}
+
+#JSON viewer URL http://jsonviewer.stack.hu/
 
 RED_PLANET_URL       =  "https://redplanetscience.com/"
 MARS_IMAGES_URL      =  "https://spaceimages-mars.com/"
 GALAXY_FACTS_URL     =  "https://galaxyfacts-mars.com/"
 MARS_HEMISPHERES_URL =  "https://marshemispheres.com/"
 
+mars_facts={}
+
 #Sergeant Function
 def scrape() -> dict:
-
     scrape_red_planet()
     scrape_mars_images()
     scrape_galaxy_facts()
     scrape_mars_hemispheres()
+    print(mars_facts)
 
 def scrape_red_planet():
-    # Set up Splinter
-    executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
 
-    # Visit visitcostarica.herokuapp.com
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser         = Browser('chrome', **executable_path, headless=False)    
+
     url = RED_PLANET_URL
     browser.visit(url)
 
@@ -32,39 +34,122 @@ def scrape_red_planet():
     # Scrape page into Soup
     html = browser.html
     soup = bs(html, "html.parser")
+    news_items = soup.find_all('div', class_='list_text')
+    news_hash = {}
 
-    # Get the average temps
-    avg_temps = soup.find('div', id='weather')
+    news_title = None
+    news_body  = None
+  
+    for news_item in news_items:
+        news_title = news_item.find('div',class_='content_title').text
+        news_body = news_item.find('div',class_="article_teaser_body").text
 
-    # Get the min avg temp
-    min_temp = avg_temps.find_all('strong')[0].text
-
-    # Get the max avg temp
-    max_temp = avg_temps.find_all('strong')[1].text
-
-    # BONUS: Find the src for the sloth image
-    relative_image_path = soup.find_all('img')[2]["src"]
-    sloth_img = url + relative_image_path
-
-    # Store data in a dictionary
-    costa_data = {
-        "sloth_img": sloth_img,
-        "min_temp": min_temp,
-        "max_temp": max_temp
-    }
-
-    # Close the browser after scraping
-    browser.quit()
-
-    # Return results
-    return costa_data
-
+        news_hash.update({news_title:news_body})
+        mars_facts.update({"latest_mars_news":news_hash})
+        break #only take first news item
 
 def scrape_mars_images():
-    None
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser         = Browser('chrome', **executable_path, headless=False)  
+    featured_image_hash = {}
+
+    browser.visit(MARS_IMAGES_URL)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+
+    mars_images_url     = browser.url
+    mars_image_wrapper  = soup.find_all('div', class_='floating_text_area')[0]
+    img_target_url      = mars_image_wrapper.find('a')['href']
+    featured_image_url  = mars_images_url + img_target_url
+    featured_image_hash = {"featured_image":featured_image_url}
+    mars_facts.update({"featured_image":featured_image_hash})
 
 def scrape_galaxy_facts():
-    None
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser         = Browser('chrome', **executable_path, headless=False)    
+
+    url = GALAXY_FACTS_URL
+    browser.visit(url)
+
+    time.sleep(1)
+
+    # Scrape page into Soup
+    html = browser.html
+    soup = bs(html, "html.parser")    
+
+    equitorial_diameter = "Equatorial Diameter"
+    polar_diameter      = "Polar Diameter"
+    mass                = "Mass"
+    moon                = "Moons"
+    orbit_distance      = "Orbit Distance"
+    orbit_period        = "Orbit Period"
+    surface_temperature = "Surface Temperature"
+    first_record        = "First Record"
+    recorded_by         = "Recorded By"
+
+    mars_fact_hash = {
+                        equitorial_diameter:None,
+                        polar_diameter:None,
+                        mass:None,
+                        moon:None,
+                        orbit_distance:None,
+                        orbit_period:None,
+                        surface_temperature:None,
+                        first_record:None,
+                        recorded_by:None
+                    }
+
+    mars_facts_wrapper = soup.find('table', class_='table-striped')
+    body = mars_facts_wrapper.find('tbody')
+    table_rows = body.find_all('tr')
+
+    for table_row in table_rows:
+        measurement_key   = table_row.find('th').text.replace(':','')
+        measurement_value = table_row.find('td').text.replace(':','')
+        mars_fact_hash.update({measurement_key:measurement_value})
+    
+    mars_facts.update({"mars_facts":mars_fact_hash})
 
 def scrape_mars_hemispheres():
-    None
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser         = Browser('chrome', **executable_path, headless=False)    
+
+    browser.visit(MARS_HEMISPHERES_URL)
+    html = browser.html
+    soup = bs(html, 'html.parser')
+    
+    hemisphere_image_urls = []
+
+    time.sleep(1)
+
+    mars_hemispheres_wrapper = soup.find('div', class_='collapsible results')
+    hemispheres              = mars_hemispheres_wrapper.find_all('div', class_='item')
+    mars_hemispheres_url     = browser.url
+
+    for hemisphere in hemispheres:
+        title               = hemisphere.find('h3').text.replace('Enhanced','')
+        large_image_nav_url = mars_hemispheres_url + hemisphere.find('a')['href']
+
+        browser.visit(large_image_nav_url)
+        html = browser.html
+        soup = bs(html, 'html.parser')
+
+        mars_large_image_wrapper = soup.find('div', class_='downloads')
+        image_container = mars_large_image_wrapper.find('ul')
+        image_items = image_container.find_all('li')
+
+        for image_item in image_items:
+            image_pict_desc = image_item.find('a').text
+
+            if 'Sample' == image_pict_desc:
+                img_url = mars_hemispheres_url + image_item.find('a')['href']
+                hemisphere_image_urls.append({title:img_url})
+                break
+
+    mars_facts.update({'hemisphere_image_urls' : hemisphere_image_urls})
+
+#My function to call scrape as a test
+#scrape()
